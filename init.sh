e74f4e028141e1acbc5d71c24761b6499cf8c482
arth.moon <arth.moon85@gmail.com>
2020-01-05 14:51:39 +0900

    init3

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@
 cd /home/pi
 git clone https://github.com/arthmoon/rpi.git
 cp /home/pi/rpi/water.py /home/pi/rpi/sync.py /home/pi/rpi/sync.sh /home/pi/rpi/tmp /home/pi/
-sudo chown pi:pi /home/pi/sync.py /home/pi/sync.sh /home/pi/water.py
-sudo chmod +x /home/pi/sync.sh
+chown pi:pi /home/pi/sync.py /home/pi/sync.sh /home/pi/water.py
+chmod +x /home/pi/sync.sh
 cp /home/pi/rpi/water.rules /etc/udev/rules.d/
-sudo cp /home/pi/rpi/water.desktop /etc/xdg/autostart/
+cp /home/pi/rpi/water.desktop /etc/xdg/autostart/
 crontab /home/pi/rpi/crontab.txt
